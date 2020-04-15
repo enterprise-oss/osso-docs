a636ad0479cc1ef7d46f28ecbcef1a680eed6b2a
Sam Bauch <sbauch@gmail.com>
2020-04-15 08:32:48 -0400

    init commit

---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: 'SAMLBox',
+  tagline: 'Authenticate Single Sign On users via SAML',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'enterpris-oss', // Usually your GitHub org/user name.
+  projectName: 'saml-box', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'SAMLBox',
+      logo: {
+        alt: 'SAMLBox logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        {
+          to: 'docs/doc1',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: <>Treat SAML as OAuth</>,
+    imageUrl: 'img/catch_bigger.png',
+    description: (
+      <>
+        SAMLBox authenticates users against SAML Identity Provider services,
+        normalizes user payloads, and provides an OAuth server to sign those
+        users into your application.
+      </>
+    ),
+  },
+  {
+    title: <>Configuration Boilerplate</>,
+    imageUrl: 'img/build_vs_buy.png',
+    description: (
+      <>
+        SAMLBox provides an admin UI, configuration API, and React components you can use in your
+        own application to onboard enterprise accounts and configure their SAML Identity Provider.
+      </>
+    ),
+  },
+  {
+    title: <>Docs for Your Whole Team</>,
+    imageUrl: 'img/build_vs_buy.png',
+    description: (
+      <>
+        Beyond technical docuemtnation, SAMLBox provides guides and
+        docs for your sales and success teams and your enterprise
+        customers to make onboarding SAML accounts smooth sailing.
+      </>
+    ),
+  },
+];
+
+function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={classnames('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="A free open source SSO and SAML microservice">
+      <header className={classnames('hero', styles.heroBanner)}>
+        <div className={styles.fish}>
+          <img className={styles.fish1} src="img/fish.svg" />
+        </div>
+        <div className={styles.fish}>
+          <img className={styles.fish2} src="img/fish2.svg" />
+        </div>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={classnames(
+                'button button--primary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/doc1')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
