f59d64eca615820383caede20bf03b1614b226d8
Sam Bauch <sbauch@gmail.com>
2020-09-17 11:04:54 -0400

    add email form in footer

---
+++ netlify-forms/index.js
@@ -0,0 +1,15 @@
+module.exports = function (context, options) {
+  return {
+    name: 'netlify-forms',
+    injectHtmlTags() {
+      return {
+        headTags: [],
+        preBodyTags: [
+          `<form name="email-list" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /></form>`
+
+        ],
+        postBodyTags: [],
+      };
+  }
+  };
+};
--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -11,6 +11,7 @@ import { useMediaQuery } from 'react-responsive'
 
 import screens from '../../utils/responsive';
 import styles from './styles.module.css';
+import { Form, Input, Button } from 'antd';
 
 function Footer() {
   const isLargeScreen = useMediaQuery({ query: screens.large })
@@ -23,6 +24,8 @@ function Footer() {
           {isLargeScreen && (
             <div className={styles.footerCol}>
               <a href="mailto:hello@enterpriseoss.dev">hello@enterpriseoss.dev</a>
+              <p>Osso by EnterpriseOSS, Inc.</p>
+              <p>Brooklyn, NY</p>
             </div>
           )}
           <div className={styles.footerCol}>
@@ -30,8 +33,22 @@ function Footer() {
           </div>
           {isLargeScreen && (
             <div className={styles.footerCol}>
-              <p>Osso by EnterpriseOSS, Inc.</p>
-              <p>Brooklyn, NY</p>
+             <Form
+              colon={false}
+              hideRequiredMark
+             >
+              <Form.Item
+                label="Enter your email to receive updates from Osso"
+                name="email"
+                rules={[{ required: true, message: 'Please input your email' }]}
+              >
+                <Input.Group compact>
+                  <Input type="email" style={{ width: '220px' }} />
+                  <Button>Submit</Button>
+                </Input.Group>
+              </Form.Item>
+              <input type="hidden" name="form-name" value="email-list" />
+             </Form>
             </div>
           )}
         </div>
