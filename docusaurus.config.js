bc2ad13d61769ba8c45eaa20e47dc38c2c3c6c85
Sam Bauch <sbauch@gmail.com>
2020-10-01 11:45:18 -0400

    try using plugin again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,84 +1,82 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  title: 'Osso',
-  tagline: 'Authenticate Single Sign On users via SAML',
-  url: 'https://ossoapp.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'enterprise-oss',
-  projectName: 'osso-docs',
+  title: "Osso",
+  tagline: "Authenticate Single Sign On users via SAML",
+  url: "https://ossoapp.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "enterprise-oss",
+  projectName: "osso-docs",
   stylesheets: [
-    'https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap',
-    'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap'
+    "https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@600&display=swap",
+    "https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap",
   ],
   themeConfig: {
     algolia: {
-      apiKey: 'e446f4b89c6d2f86db157cc2906a1520',
-      indexName: 'ossoapp',
+      apiKey: "e446f4b89c6d2f86db157cc2906a1520",
+      indexName: "ossoapp",
       searchParamaters: {
-        appId: 'BH4D9OD16A',
+        appId: "BH4D9OD16A",
       },
     },
     announcementBar: {
-      id: 'support_us', // Any value that will identify this message.
+      id: "support_us", // Any value that will identify this message.
       content:
         'Osso v1.0.0.rc is out! 🎉 🍾 Read our <a href="/blog/1-0-0-release-candidate">launch blog post</a>.',
-      backgroundColor: '#fafbfc', // Defaults to `#fff`.
-      textColor: '#091E42', // Defaults to `#000`.
+      backgroundColor: "#fafbfc", // Defaults to `#fff`.
+      textColor: "#091E42", // Defaults to `#000`.
       isCloseable: false, // Defaults to `true`.
     },
     gtag: {
-      trackingID: 'UA-177630158-1',
+      trackingID: "UA-177630158-1",
     },
     navbar: {
-      title: 'Osso',
+      title: "Osso",
       items: [
         {
-          to: 'docs/integration/overview',
-          activeBasePath: 'docs/integration',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/integration/overview",
+          activeBasePath: "docs/integration",
+          label: "Docs",
+          position: "left",
         },
         {
-          to: 'docs/what-is-saml',
-          activeBasePath: 'guides',
-          label: 'Guides',
-          position: 'left',
+          to: "docs/what-is-saml",
+          activeBasePath: "guides",
+          label: "Guides",
+          position: "left",
         },
-        { to: 'blog', label: 'Blog', position: 'left' },
-        { to: 'pricing', label: 'Pricing', position: 'left' },
+        { to: "blog", label: "Blog", position: "left" },
+        { to: "pricing", label: "Pricing", position: "left" },
         {
-          href: 'https://github.com/enterprise-oss/osso',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'GitHub repository',
+          href: "https://github.com/enterprise-oss/osso",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
-          items: [
-           
-          ],
+          title: "Docs",
+          items: [],
         },
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/enterprise-oss/osso',
+              label: "GitHub",
+              href: "https://github.com/enterprise-oss/osso",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/osso',
+              label: "Twitter",
+              href: "https://twitter.com/osso",
             },
           ],
         },
@@ -88,26 +86,27 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/enterprise-oss/osso-docs/edit/master/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/enterprise-oss/osso-docs/edit/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
   plugins: [
-    [require.resolve("docusaurus-plugin-less"), {
-      lessOptions: {
+    [
+      require.resolve("docusaurus-plugin-less"),
+      {
+        lessOptions: {
           javascriptEnabled: true,
-      }
-    },
+        },
+      },
     ],
-    // path.resolve(__dirname, 'netlify-forms'),
-  ]
+    path.resolve(__dirname, "netlify-forms"),
+  ],
 };
--- netlify-forms/index.js
+++ netlify-forms/index.js
@@ -1,14 +1,15 @@
 module.exports = function (context, options) {
   return {
-    name: 'netlify-forms',
+    name: "netlify-forms",
     injectHtmlTags() {
       return {
         headTags: [],
         preBodyTags: [
-          `<form name="email-list" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /></form>`
+          `<form name="email-list" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /></form>`,
+          `<form name="plan-interest" netlify netlify-honeypot="bot-field" hidden><input type="email" name="email" /><input name="about" /><input name="chosenPlan" /></form>`,
         ],
         postBodyTags: [],
       };
-  }
+    },
   };
 };
