f29746437b2868e6be9690288c948d8eca93384c
Sam Bauch <sbauch@gmail.com>
2020-10-01 10:09:19 -0400

    modal form

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -9,14 +9,14 @@ import screens from '../../utils/responsive';
 import {
   CheckCircleFilled,
 } from '@ant-design/icons';
-import { Button, Card, Col, Row } from 'antd'
+import { Button, Card, Divider, Col,Form , Input, Layout as AntLayout,Modal, Row } from 'antd'
 
 const plans = [
   { 
     name: 'Community',
     price: "$0",
     className: '',
-    cta: 'Learn More',
+    cta: 'Start for free',
     features: [
       "Deploy on your own infrastructure",
       "Community support via community.ossoapp.com",
@@ -24,13 +24,18 @@ const plans = [
       "Admin UI to manage Enterprise SSO users",
     ],
     styles: {
-    }
+      borderRadius: 4,
+    },
+    titleStyles: {
+      borderRadius: '4px 4px 0 0 ',
+    },
+    markerColor: "#4E61A5",
   },
   { 
     name: 'Business',
-    price: "$199/mo",
+    price: "$249/mo",
     className: 'businessCard',
-    cta: 'Learn More',
+    cta: 'Get started',
     features: [
       "Everything in the Community plan",
       "Quick start: deploy on our infrastructure",
@@ -42,13 +47,19 @@ const plans = [
     ],
     styles: {
       borderColor: "#4E61A5",
-    }
+      boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.15)',
+      borderRadius: 4,
+    },
+    titleStyles: {
+      borderRadius: '4px 4px 0 0 ',
+    },
+    markerColor: "#4E61A5",
   },
   { 
     name: 'Enterprise',
     price: "Custom",
     className: 'enterpriseCard',
-    cta: 'Learn More',
+    cta: 'Contact us',
     features: [
       "Everything in the Business plan",
       "Video & phone support",
@@ -63,87 +74,150 @@ const plans = [
     styles: {
       backgroundColor: "#4E61A5",
       color: 'white',
-    }
+      boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.15)',
+      borderRadius: 4,
+    },
+    titleStyles: {
+      color: 'white',
+      borderRadius: '4px 4px 0 0 ',
+    },
+    markerColor: "white",
   }
 ]
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   const isLargeScreen = useMediaQuery({ query: screens.large })
+  const [chosenPlan, setChosenPlan] = useState('');
+  const [form] = Form.useForm();
+  const domForm = useRef();
 
   return (
     <Layout
       title={siteConfig.title}
       description="Free, open-source software for adding SAML based SSO to your application">
-          <div className="container margin-vert--xl ">
-            <Row gutter={[16, 72]}>
-              <Col className={styles.features}>
-                <h3>
-                  {isLargeScreen && <span className={styles.titleMarker} />}
-                  Osso is for everybody
-                </h3>
-                <p>
-                  Osso is and will remain free, open source software that anyone can use to authenticate users to their app with SSO. 
-                  <br/><br/>
-                  If you want to reduce the integration load for your engineering team, or appreciate the peace of mind 
-                  that comes with personalized support, we're happy to offer private managed instances on a subscription basis. 
-                  We can also consult with your engineering team and help train your sales and customer success teams.
-                  If that sounds interesting, read on.  
-                </p>
-              </Col>
-            </Row>
-            <Row gutter={[16, 48]}>
-              <Col className={styles.features}>
-                <h3>
-                  {isLargeScreen && <span className={styles.titleMarker} />}
-                  Our plans
-                </h3>
-              </Col>
-            </Row>
-            <Row gutter={[24, 72]}>
-              { plans.map((plan) => (
-                <Col sm={24} lg={8}>
-                  <Card 
-                    className={[styles.planCard, styles[plan.className]]}
-                    title={
-                      <div className={styles.planHeader}>
-                        <span>{plan.name}</span>
-                        <span>{plan.price}</span>
-                      </div>
-                    }
-                    bodyStyle={{
-                      display: 'flex',
-                      flexGrow: "1",
-                      flexDirection: 'column',
-                      ...plan.styles,
-                    }}
-                  >
-                    { plan.features.map((feature) => (
-                      <p className={styles.planFeature}>
-                        <CheckCircleFilled style={{marginRight: 4, color: "#4E61A5"}}/>
-                        {feature}
-                      </p>
-                    ))}
-
-                    <Button style={{marginTop: 'auto', marginBottom: 12,}}>{plan.cta}</Button>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-              <Row>
-                <div className={classnames('col', styles.features)}>
-                  <h3>
-                    {isLargeScreen && <span className={styles.titleMarker} />}
-                    Not ready to commit?
-                  </h3>
-                  <p>
-                    If we’re missing something that could be a big help to you or your organization, get in touch and let us know. We’re scrappy and we want your business. 
-
-                    If you’d like to follow along as we release new features, give us your email address and we’ll keep you in the loop; no SPAM and no sharing of your information with any other entity.
-                  </p>
+    <AntLayout.Content className="container margin-vert--xl ">
+      <Row gutter={[16, 72]}>
+        <Col sm={24} md={{span: 20, offset: 2}} className={styles.features}>
+          <h3>
+            {isLargeScreen && <span className={styles.titleMarker} />}
+            Osso is for everybody
+          </h3>
+          <p>
+            Osso is and will remain free, open source software that anyone can use to authenticate users to their app with SSO. 
+            <br/><br/>
+            If you want to reduce the integration load for your engineering team, or appreciate the peace of mind 
+            that comes with personalized support, we're happy to offer private managed instances on a subscription basis. 
+            We can also consult with your engineering team and help train your sales and customer success teams.
+            If that sounds interesting, read on.  
+          </p>
+        </Col>
+      </Row>
+      <Row gutter={[16, 48]}>
+        <Col sm={24} md={{span: 20, offset: 2}} className={styles.features}>
+          <h3>
+            {isLargeScreen && <span className={styles.titleMarker} />}
+            Our plans
+          </h3>
+        </Col>
+      </Row>
+      <Row gutter={[24, 72]}>
+        { plans.map((plan) => (
+          <Col sm={24} lg={8}>
+            <Card 
+              className={[styles.planCard, styles[plan.className]]}
+              title={
+                <div className={styles.planHeader}>
+                  <span>{plan.name}</span>
+                  <span>{plan.price}</span>
                 </div>
-              </Row>
-            </div>
+              }
+              style={plan.styles}
+              headStyle={plan.titleStyles}
+              bodyStyle={{
+                display: 'flex',
+                flexGrow: "1",
+                flexDirection: 'column',
+                ...plan.bodyStyles,
+              }}
+            >
+              <div className={styles.featureList}>
+                { plan.features.map((feature) => (
+                  <p className={styles.planFeature}>
+                    <CheckCircleFilled className={styles.planFeatureMarker} style={{color: plan.markerColor }} />
+                    {feature}
+                  </p>
+                ))}
+              </div>
+
+              <Button 
+                onClick={plan.name !== 'Community' && (() => setChosenPlan(plan.name))}
+                href={plan.name === 'Community' ? useBaseUrl('docs/what-is-saml') : undefined}
+                style={{marginTop: 'auto', marginBottom: 12,}}
+                >
+                  {plan.cta}
+                </Button>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+        <Row>
+          <Col sm={24} md={{span: 20, offset: 2}} className={styles.features}>
+            <h3>
+              {isLargeScreen && <span className={styles.titleMarker} />}
+              Not ready to commit?
+            </h3>
+            <p>
+              If we’re missing something that could be a big help to you or your organization, get in touch and let us know. We’re scrappy and we want your business. 
+              <br/><br/>
+              If you’d like to follow along as we release new features, give us your email address and we’ll keep you in the loop; no SPAM and no sharing of your information with any other entity.
+            </p>
+          </Col>
+        </Row>
+        <Modal
+          visible={Boolean(chosenPlan)}
+          title="Coming Soon"
+          width={640}
+          onCancel={() => setChosenPlan('')}
+          onOk={() => {
+            form
+              .validateFields()
+              .then(values => {
+                form.resetFields();
+                console.log(domForm.current)
+                // onCreate(values);
+              })
+              .catch(info => {
+                console.log('Validate Failed:', info);
+              });
+          }}
+        >
+          <h3>Thanks for your interest in our {chosenPlan} plan! </h3>
+          <p>
+            We’re hard at work building out our self-service offering and will keep you in the loop as soon as it’s ready.
+            Give us your email (we won’t share it with anybody else, ever) and you’ll be the first to know when it’s available.
+          </p>
+          <Form ref={domForm} hideRequiredMark form={form} layout="vertical" validateTrigger="submit">
+            <Form.Item
+              label="Email"
+              name="email"
+              type="email"
+              rules={[{ type: 'email', required: true, message: 'Please add your work email' }]}
+            >
+                <Input />
+            </Form.Item>
+            <Divider />
+            <p>
+            If you’d like to be considered for our beta cohort, tell us a bit more about your company, your tech stack, and where SSO fits into your roadmap and we’ll be in touch. (Optional)
+            </p>
+            <Form.Item>
+              <Input.TextArea placeholder="My company is..."/>
+            </Form.Item>
+          </Form>                    
+
+      </Modal>   
+      </AntLayout.Content>
+                 
     </Layout >
   );
 }
