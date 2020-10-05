c5c1a464074e97c055c684f567a36702929be109
Sam Bauch <sbauch@gmail.com>
2020-10-05 09:59:18 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/enterprise-oss/osso-docs/edit/master/",
+          editUrl: "https://github.com/enterprise-oss/osso-docs/edit/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
