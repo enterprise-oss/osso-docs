9c5af602eb15dcff78a4bb22beb37b11015e4a65
Sam Bauch <sbauch@gmail.com>
2020-10-08 15:15:08 -0400

    lint

--- sidebars.js
+++ sidebars.js
@@ -1,34 +1,34 @@
 module.exports = {
   someSidebar: [
-    'what-is-saml',
-    'overview',
+    "what-is-saml",
+    "overview",
     {
-      type: 'category',
-      label: 'Integration Docs',
+      type: "category",
+      label: "Integration Docs",
       items: [
-        'integration/overview',
-        'integration/deployment',
-        'integration/jwt-authentication',
-        'integration/oauth-consumption',
-        'integration/test',
-        'integration/self-serve-osso-react',
-      ]
+        "integration/overview",
+        "integration/deployment",
+        "integration/jwt-authentication",
+        "integration/oauth-consumption",
+        "integration/test",
+        "integration/self-serve-osso-react",
+      ],
     },
     {
-      type: 'category',
-      label: 'Usage Guides',
+      type: "category",
+      label: "Usage Guides",
       items: [
         {
-          type: 'category',
-          label: 'Admin UI',
-          items: ['usage/admin-ui/overview'],
+          type: "category",
+          label: "Admin UI",
+          items: ["usage/admin-ui/overview"],
         },
         {
-          type: 'category',
-          label: 'Identity Providers',
-          items: ['idp/overview', 'idp/okta',]
+          type: "category",
+          label: "Identity Providers",
+          items: ["idp/overview", "idp/okta"],
         },
-      ]
+      ],
     },
   ],
 };
