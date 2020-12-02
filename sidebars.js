b1809d862f245a6306599c27ad0c5e35df3e64c0
Sam Carmichael <scarmichael@sams-mbp.myfiosgateway.com>
2020-12-02 14:26:36 -0500

    update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,44 @@
 module.exports = {
   someSidebar: [
-    "what-is-saml",
-    "overview",
     {
       type: "category",
-      label: "Integration Docs",
+      label: "Quick start",
       items: [
-        "integration/overview",
-        "integration/deployment",
-        "integration/oauth-consumption",
-        "integration/test",
-        "integration/self-serve-osso-react",
+        "quick-start/overview",
+        "quick-start/deploy",
+        "quick-start/consume",
+        "quick-start/sign-in",
+        "quick-start/handoff",
+      ],
+    },
+    {
+      type: "category",
+      label: "How to deploy Osso",
+      items: [
+        "deploy/overview",
+        "deploy/with-heroku",
+        "deploy/with-docker",
+        "deploy/env-variables",
+        "deploy/post-deployment",
+      ],
+    },
+    {
+      type: "category",
+      label: "How to configure Osso",
+      items: [
+        "configure/overview",
+        "configure/adding-oauth-clients",
+        "configure/redirect-uris",
+      ],
+    },
+    {
+      type: "category",
+      label: "How to use Osso",
+      items: [
+        "user-guide/overview",
+        "user-guide/adding-customers",
+        "user-guide/onboarding-customers",
+        "user-guide/self-serve",
       ],
     },
     {
