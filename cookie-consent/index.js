a2aaabe8dcace7f20b25794a26abfe63f56081cc
Sam Bauch <sbauch@gmail.com>
2020-12-24 09:53:25 -0500

    customize widget

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -6,7 +6,7 @@ module.exports = function (context, options) {
         headTags: [
           `<script type="text/javascript">
           var _iub = _iub || [];
-          _iub.csConfiguration = {"consentOnContinuedBrowsing":false,"ccpaAcknowledgeOnDisplay":true,"whitelabel":false,"lang":"en","siteId":2103664,"enableCcpa":true,"countryDetection":true,"cookiePolicyId":28156222, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-top-center" }};
+          _iub.csConfiguration = {"enableCcpa":true,"countryDetection":true,"consentOnContinuedBrowsing":false,"ccpaAcknowledgeOnDisplay":true,"lang":"en","siteId":2103664,"floatingPreferencesButtonDisplay":false,"gdprAppliesGlobally":false,"cookiePolicyId":28156222,"floatingPreferencesButtonCaption":true, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-bottom-right","logo":"https://deploy-preview-34--osso.netlify.app/img/logo-alt.svg","acceptButtonColor":"#4E61A5","fontSize":"16px","brandBackgroundColor":"none","brandTextColor":"#ffffff","acceptButtonCaptionColor":"white","customizeButtonColor":"#DADADA","customizeButtonCaptionColor":"#4D4D4D","rejectButtonColor":"#4e61a5","rejectButtonCaptionColor":"white","textColor":"#ffffff","backgroundColor":"#262626" }};
           </script>
           <script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js"></script>
           <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>`,
