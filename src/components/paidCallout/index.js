d0e22c2c16e8e7e65045a3070d3d786d08400e57
Sam Bauch <sbauch@gmail.com>
2021-01-08 11:57:57 -0500

    add paid callout

---
+++ src/components/paidCallout/index.js
@@ -0,0 +1,22 @@
+import { SmileTwoTone, SoundTwoTone } from "@ant-design/icons";
+import Link from "@docusaurus/Link";
+import React from "react";
+
+import styles from "./styles.module.less";
+
+export default function paidCallout() {
+  return (
+    <div className={styles.root}>
+      <div className={styles.icons}>
+        <SmileTwoTone twoToneColor="#4E61A5" />
+        <SoundTwoTone twoToneColor="#4E61A5" />
+      </div>
+      <span className={styles.message}>
+        The content of this article applies to the open source version of Osso
+        and is not relevant to our <Link to="/pricing">paid plans</Link>. If
+        you&apos;re already an Osso customer,{" "}
+        <Link to="/docs/configure/overview">skip ahead to Configuration</Link>.
+      </span>
+    </div>
+  );
+}
