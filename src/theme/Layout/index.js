6917c1c832fd4802501ad7025f13c7e3f6e26e40
Sam Bauch <sbauch@gmail.com>
2021-01-13 15:29:54 -0500

    customize layout for ant theme

---
+++ src/theme/Layout/index.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import "./styles.css";
+import "@enterprise-oss/ant-theme";
+
+import AnnouncementBar from "@theme/AnnouncementBar";
+import Footer from "@theme/Footer";
+import useKeyboardNavigation from "@theme/hooks/useKeyboardNavigation";
+import LayoutHead from "@theme/LayoutHead";
+import LayoutProviders from "@theme/LayoutProviders";
+import Navbar from "@theme/Navbar";
+import SkipToContent from "@theme/SkipToContent";
+import clsx from "clsx";
+import React from "react";
+
+function Layout(props) {
+  const { children, noFooter, wrapperClassName } = props;
+  useKeyboardNavigation();
+  return (
+    <LayoutProviders>
+      <LayoutHead {...props} />
+
+      <SkipToContent />
+
+      <AnnouncementBar />
+
+      <Navbar />
+
+      <div className={clsx("main-wrapper", wrapperClassName)}>{children}</div>
+
+      {!noFooter && <Footer />}
+    </LayoutProviders>
+  );
+}
+
+export default Layout;
