968265afdce58029d7dd3a6e3a442229041ddf91
Sam Bauch <sbauch@gmail.com>
2021-01-13 17:19:50 -0500

    darm mode for callouts

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -4,8 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import "./styles.css";
 import "@enterprise-oss/ant-theme";
+import "./styles.css";
 
 import AnnouncementBar from "@theme/AnnouncementBar";
 import Footer from "@theme/Footer";
