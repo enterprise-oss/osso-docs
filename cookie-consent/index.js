445201e431febae765041cf7bc91a65a95ef6cef
Sam Bauch <sbauch@gmail.com>
2021-01-14 13:41:05 -0500

    SAML vs OAuth (#38)

--- cookie-consent/index.js
+++ cookie-consent/index.js
@@ -4,12 +4,12 @@ module.exports = function (context, options) {
     injectHtmlTags() {
       return {
         headTags: [
-          `<script async type="text/javascript">
+          `<script async defer type="text/javascript">
           var _iub = _iub || [];
           _iub.csConfiguration = {"enableCcpa":true,"countryDetection":true,"consentOnContinuedBrowsing":false,"ccpaAcknowledgeOnDisplay":true,"lang":"en","siteId":2103664,"floatingPreferencesButtonDisplay":false,"gdprAppliesGlobally":false,"cookiePolicyId":28156222,"floatingPreferencesButtonCaption":true, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"position":"float-bottom-right","logo":"https://deploy-preview-34--osso.netlify.app/img/logo-alt.svg","acceptButtonColor":"#4E61A5","fontSize":"16px","brandBackgroundColor":"none","brandTextColor":"#ffffff","acceptButtonCaptionColor":"white","customizeButtonColor":"#DADADA","customizeButtonCaptionColor":"#4D4D4D","rejectButtonColor":"#4e61a5","rejectButtonCaptionColor":"white","textColor":"#ffffff","backgroundColor":"#262626" }};
           </script>
-          <script type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js"></script>
-          <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>`,
+          <script async defer type="text/javascript" src="//cdn.iubenda.com/cs/ccpa/stub.js"></script>
+          <script async defer type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>`,
         ],
         preBodyTags: [],
         postBodyTags: [],
