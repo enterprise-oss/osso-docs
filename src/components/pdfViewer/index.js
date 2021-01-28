2f4f02d26741b584813a333d001ab4f67ffe163a
Sam Bauch <sbauch@gmail.com>
2021-01-28 17:55:28 -0500

    toggle with effect

--- src/components/pdfViewer/index.js
+++ src/components/pdfViewer/index.js
@@ -1,12 +1,16 @@
 import CloudPdfViewer from "@openbook/cloudpdf-viewer";
 import useThemeContext from "@theme/hooks/useThemeContext";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import styles from "./styles.module.css";
 
 export default function PdfViewer({ documentId }) {
   const [instance, setInstance] = useState(null);
   const { isDarkTheme } = useThemeContext();
+
+  useEffect(() => {
+    instance.setDarkMode(isDarkTheme);
+  }, [isDarkTheme]);
 
   const measuredRef = useCallback((node) => {
     if (node !== null && !instance) {
