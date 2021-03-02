9007eb1a34cd53248a05a26c8255e216b004bc36
Sam Bauch <sbauch@gmail.com>
2021-03-02 13:22:56 -0500

    getting there

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import classnames from "classnames";
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 
+import BlogTeaser from "../components/blogTeaser";
 import AdminUI from "../components/svg/AdminUI.svg";
 import OssoDiagram from "../components/svg/OssoDiagram.svg";
 import PDFs from "../components/svg/PDFs.svg";
@@ -19,20 +20,38 @@ const Separator = () => (
   </svg>
 );
 
+const customSortedIDPs = (idps) => {
+  const ordering = {};
+  const sortOrder = [
+    "AZURE",
+    "OKTA",
+    "ONELOGIN",
+    "SALESFORCE",
+    "GOOGLE",
+    "PING",
+    "OTHER",
+  ];
+
+  for (var i = 0; i < sortOrder.length; i++) ordering[sortOrder[i]] = i;
+
+  return idps.sort((a, b) => ordering[a.value] - ordering[b.value]);
+};
+
 function Home() {
   const isLargeScreen = useMediaQuery({ query: screens.large });
   const { providers } = useOssoFields();
   return (
-    <Layout title="Home" noNavbar>
-      <div className={styles.hero}>
-        <h1>Add enterprise-grade SSO to any application</h1>
-        <Link to={useBaseUrl("docs/quick-start")}>
-          <Button ghost size="large" className={styles.heroBtn}>
-            Get started
-          </Button>
-        </Link>
+    <Layout title="Home" noNavbar={isLargeScreen}>
+      <div className={styles.heroWrapper}>
+        <div className={styles.hero}>
+          <h1>Add enterprise-grade SSO to any application</h1>
+          <Link to={useBaseUrl("docs/quick-start")}>
+            <Button ghost size="large" className={styles.heroBtn}>
+              Get started
+            </Button>
+          </Link>
+        </div>
       </div>
-
       <div className={styles.benefit}>
         <AdminUI />
         <div>
@@ -49,7 +68,7 @@ function Home() {
       <div className={styles.alt}>
         <h2>Supported Identity Providers</h2>
         <div className={styles.providerLogos}>
-          {providers.map((provider) => (
+          {customSortedIDPs(providers).map((provider) => (
             <div key={provider.value}>
               <img
                 src={provider.iconUrl}
@@ -78,7 +97,20 @@ function Home() {
         <OssoDiagram />
       </div>
       <Separator />
-      <div className={styles.alt}></div>
+      <div className={styles.alt}>
+        <h2>SDKs and Integrations</h2>
+        <div className={styles.providerLogos}>
+          {customSortedIDPs(providers).map((provider) => (
+            <div key={provider.value}>
+              <img
+                src={provider.iconUrl}
+                className={styles.providerLogo}
+                alt={provider.label}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <Separator />
 
       <div className={styles.benefit}>
@@ -90,6 +122,28 @@ function Home() {
             in an intuitive UI, and generates custom documentation for each
             customer to set up your application in their Identity Provider.
           </p>
+        </div>
+      </div>
+      <Separator />
+      <div className={classnames(styles.alt, styles.blogRoll)}>
+        <h2>Recent Blog Posts</h2>
+        <div className={styles.blogTeasers}>
+          <BlogTeaser
+            authorImg="https://avatars1.githubusercontent.com/u/923033?s=460&u=db9bb41f9b279750c74afc1be0ab51db05539593&v=4"
+            title="Add SAML SSO to a Rails 6 App"
+            preview="SAML SSO refers to an authentication mechanism preferred by enterprise companies. The SSO part stands for Single Sign-On. From the enterprise’s perspective, they desire a centralized service where their employees can authenticate, which then provides authenticated access to the... "
+            path="/blog/saml-sso-rails"
+          />
+          <BlogTeaser
+            authorImg="https://avatars0.githubusercontent.com/u/5394293?s=460&u=03bbc0c5174d8341157e4cddb3252b74f33b2482&v=4"
+            title="The ABCs of IDPs"
+            preview="Identity Providers (IDPs) are a category of software applications responsible for managing employee access to the various third party applications (AKA Service Providers) that modern enterprise companies rely on."
+          />
+          <BlogTeaser
+            authorImg="https://avatars1.githubusercontent.com/u/923033?s=460&u=db9bb41f9b279750c74afc1be0ab51db05539593&v=4"
+            title="SAML vs. OAuth"
+            preview="OAuth and SAML are both open specifications for exchanging access credentials for a specific user between an identity provider and an application. When a user wants to sign in to an app using either SAML or OAuth, they are sent to a third party where the user must already be registered."
+          />
         </div>
       </div>
       <Separator />
